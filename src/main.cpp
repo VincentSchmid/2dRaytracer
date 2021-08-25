bb10656c2972e3e286de791e60021a8875fde00a
Vincent Schmid <puterich@gmail.com>
2021-08-25 13:42:29 +0200

    moved code into src folder

--- src/main.cpp
+++ src/main.cpp
@@ -37,8 +37,8 @@ int main(void)
     float cameraX = 0.0f;
     float cameraY = 0.0f;
 
-    Triangle triangle = Triangle({800, 415}, 200, &GLASS);
-    Circle circle = Circle({1200, 415}, 100, &GLASS);
+    Triangle triangle = Triangle({800, 415}, 200, &CRAZY_GLASS);
+    Circle circle = Circle({1200, 415}, 100, &CRAZY_GLASS);
 
     std::list<Shape*> shapesInScene = {};
     shapesInScene.push_back(&circle);
